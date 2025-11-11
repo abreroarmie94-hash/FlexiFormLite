3a07d6a3c4f1028c84ee6a0da47208fb2b5fb279
abreroarmie94-hash <abreroarmie94@gmail.com>
2025-11-11 09:54:21 +0800

    Files for Flexiform-lite
    survey builder

---
+++ Service-Worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "flexiform-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./serviceWorker.js"
+];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)));
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request))
+  );
+});
+
+self.addEventListener("activate", e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>{
+      return Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)));
+    })
+  );
+});
